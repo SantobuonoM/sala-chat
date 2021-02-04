9d704a1435aeda6435028b9905442acd515ba185
Matias <matiasantobuono@gmail.com>
2021-02-04 12:00:22 -0300

    Pimer commit chats

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,30 +1,27 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
-    window.location = 'index.html'
-    throw new Error('El nombre y sala son necesarios    ');
-};
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+}
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-
 };
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-
-
         console.log('Usuarios conectados', resp);
-    })
-
-
+    });
 
 });
-
 
 // escuchar
 socket.on('disconnect', function() {
@@ -35,35 +32,27 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
 });
 
-// Escuchar cuando un usuario entra o sale del chat
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
     console.log(personas);
-
 });
 
-
-// mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mensaje privado', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 
-
-
-})
+});
